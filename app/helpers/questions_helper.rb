e11e30ec3df962530816d55bb1df04262cf3b1cc
Shidash <shidash@shidash.com>
2013-04-26 05:11:59 -0400

    Progress bars show proper percentage for tasks completed

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,15 @@
 module QuestionsHelper
+  
+def progress(question)
+  finished = 0.0
+  total = 0.0
+  question.tasks.each do |task|
+    if task.complete
+      finished = finished + 1
+    end
+    total = total + 1
+  end
+  finished = (finished/total)*500
+end
+
 end
