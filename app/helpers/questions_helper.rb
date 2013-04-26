f679780eb81a761a84a6109d8b3a9e6a838c012d
Shidash <shidash@shidash.com>
2013-04-26 05:51:56 -0400

    Change colors of progress bars based on progress

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,6 +1,6 @@
 module QuestionsHelper
   
-def progress(question)
+def procomplete(question)
   finished = 0.0
   total = 0.0
   question.tasks.each do |task|
@@ -10,6 +10,18 @@ def progress(question)
     total = total + 1
   end
   finished = (finished/total)*500
+end
+
+def color(percent)
+  if percent <= 150
+    "progress-danger progress-striped"
+  elsif percent <= 350
+    "progress-warning progress-striped"
+  elsif percent < 500
+    "progress-success progress-striped"
+  else 
+    "progress-success"
+  end
 end
 
 end
