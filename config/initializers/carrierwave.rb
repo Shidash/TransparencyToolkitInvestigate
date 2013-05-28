e80e26331ae738899a98d47d8adfb5873fa97bca
Shidash <shidash@shidash.com>
2013-05-28 18:15:36 -0400

    Added uploading that works with Amazon S3 when in production since Heroku doesn't save uploaded files

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,29 @@
+# config/initializers/carrierwave.rb
+
+CarrierWave.configure do |config|
+
+  # Use local storage if in development or test
+  if Rails.env.development? || Rails.env.test?
+    CarrierWave.configure do |config|
+      config.storage = :file
+    end
+  end
+
+  # Use AWS storage if in production
+  if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.storage = :fog
+    end
+  end
+
+  config.fog_credentials = {
+    :provider               => 'AWS',                             # required
+    :aws_access_key_id      => '<your key goes here>',            # required
+    :aws_secret_access_key  => '<your secret key goes here>',     # required
+    :region                 => 'eu-west-1'                        # optional, defaults to 'us-east-1'
+  }
+  config.fog_directory  = '<bucket name goes here>'               # required
+  #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
+  #config.fog_public     = false                                  # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
